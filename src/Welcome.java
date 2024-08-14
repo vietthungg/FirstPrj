2c4039ad54330cea4f78aa78c4547b92e850dd3a
vietthungg <leohungviet@gmail.com>
2024-08-14 10:42:10 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+
+    public class Welcome {
+        public static void main(String[] args) {
+            System.out.println("Nguyen Viet Hung");
+            System.out.println("Welcome to DHKTPM19ATT");
+        }
+    }
+
